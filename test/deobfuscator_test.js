fb0a3079ea22ae88a4504434350a7a56157187e3
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2018-10-05 08:06:34 +0100

    Added test files and dev dependencies.

---
+++ test/deobfuscator_test.js
@@ -0,0 +1,12 @@
+const assert = require("chai").assert;
+
+describe("DeObfuscator Tests", () => {
+
+    beforeEach(function () {});
+
+    xdescribe("XX", () => {
+        it("should xx", () => {
+            assert.equal(1, 2);
+        });
+    });
+});
