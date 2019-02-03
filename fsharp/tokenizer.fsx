8d2537e67e5c745e5ff0b2b81644ff35c7db03db
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2019-02-03 12:59:52 -0800

    Removed JS code, added test F# script.

---
+++ fsharp/tokenizer.fsx
@@ -0,0 +1,27 @@
+// CMD De-Obfuscator
+// =================
+
+let (|SpecialChar|RegularChar|) chr =
+    match chr with
+    | ' ' 
+    | '"' 
+    | '^' 
+    | '\\' -> SpecialChar
+    | _    -> RegularChar
+
+type TokeniserState = { IgnoreSpecialChars: bool}
+
+let cmd = "\"Ar\"g\"u^m\"e\"n\"t\" W\"it\"h Sp\"aces\"\""
+
+let rec QuoteReader (chars: char list) state col =
+    let parserState = state.IgnoreSpecialChars
+    match (Seq.toList chars) with
+    | head :: rest ->
+        match head with
+        | SpecialChar -> QuoteReader rest { IgnoreSpecialChars = not parserState } col
+        | RegularChar -> QuoteReader rest state (List.append col [head])
+    | [] -> col
+        
+let cmdStr = Seq.toList (cmd.ToCharArray())
+let output = QuoteReader cmdStr { IgnoreSpecialChars = false } []
+printfn "OUT -----> %A" output
