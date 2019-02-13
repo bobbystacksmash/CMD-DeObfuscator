2635ca273e1c183c88fd684aa6ddaeba7698e779
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2019-02-13 06:03:14 +0000

    Added escape handler.

--- Deobfuscator/Tokeniser.fs
+++ Deobfuscator/Tokeniser.fs
@@ -5,16 +5,18 @@ type private CharMatcher =
     | IgnoringSpecialChars
 
 type private ReaderState = {
+    Escape: bool
     Mode: CharMatcher
 }
 
 type TokenChar =
+    | EscapeChar  of char
     | SpecialChar of char
     | RegularChar of char
 
 module Tokeniser =
 
-    let (|SpecialChar|RegularChar|) chr =
+    let (|SpecialChar|RegularChar|EscapeChar|) chr =
         match chr with
         | ' '
         | '"'
@@ -25,8 +27,8 @@ module Tokeniser =
         | '>'
         | '&'
         | '|'
-        | '^'
         | '\\' -> SpecialChar chr
+        | '^'  -> EscapeChar  chr
         | _    -> RegularChar chr
 
 
@@ -34,6 +36,12 @@ module Tokeniser =
         match cmdstr with
         | chr :: rest ->
             match chr with
+            | EscapeChar escape ->
+                tokeniser rest { ctx with Escape = true } col
+
+            | _ when ctx.Escape ->
+                tokeniser rest { ctx with Escape = false} (List.append col [RegularChar(chr)])
+
             | SpecialChar special ->
                 tokeniser rest ctx (List.append col [SpecialChar(chr)])
 
@@ -45,5 +53,5 @@ module Tokeniser =
 
     let tokenise (cmdstr: string) =
         let cmdstrSeq = cmdstr.ToString() |> Seq.toList
-        tokeniser cmdstrSeq { Mode = MatchingSpecialChars } []
+        tokeniser cmdstrSeq { Mode = MatchingSpecialChars; Escape = false } []
 
