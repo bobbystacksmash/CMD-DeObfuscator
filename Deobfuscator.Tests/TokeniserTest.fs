5716a82a269f3dd65cd9c3f600399ebb3aad1449
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2019-02-14 04:55:46 +0000

    Added quote and escape tests.

--- Deobfuscator.Tests/TokeniserTest.fs
+++ Deobfuscator.Tests/TokeniserTest.fs
@@ -40,23 +40,22 @@ type TestClass () =
     [<Test>]
     member this.StandardCommandTokenising() =
 
-        let trd (_, _, t) = t
-
-        // Tuple structure is:
-        //   (INPUT, EXPECTED_OUTPUT, MSG)
-        //
         let tests = [
             ("calc.exe", ["Rc" ; "Ra"; "Rl" ; "Rc" ; "R." ; "Re" ; "Rx" ; "Re"], "Regular char tokenising.")
             ("c^alc", ["Rc" ; "Ra" ; "Rl" ; "Rc"], "Escape tokens are dropped.")
             ("&", ["S&"], "Identify a known special char.")
             ("^&", ["R&"], "Escape a known special char.")
             ("^^", ["R^"], "Escape the escape char.")
+            ("^\"", ["S\""], "Cannot escape a double quote.")
+            ("^\"ab^\"", ["S\"" ; "Ra" ; "Rb" ; "R^"; "S\""], "Cannot escape an opening or closing double quote.")
             ("(foo)", ["S(" ; "Rf" ; "Ro" ; "Ro" ; "S)"], "Identify parens.")
             (
                 "\"!()|?&%><\"",
                 ["S\"" ; "R!" ; "R(" ; "R)" ; "R|" ; "R?" ; "R&" ; "R%" ; "R>" ; "R<" ; "S\""],
                 "Quotes disable special char identification."
             )
+            ("ab\"\"cd", ["Ra" ; "Rb" ; "S\"" ; "S\"" ; "Rc" ; "Rd"], "Empty dquote resets special char matcher.")
+            ("\"\"\"", ["S\"" ; "S\"" ; "S\""], "Handling lines of quotes.")
         ]
 
         tests |> List.iter (fun test ->
