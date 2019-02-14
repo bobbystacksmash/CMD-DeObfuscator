c8b56c8147ad2aa27d151be5e928b2696b40b153
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2019-02-14 04:42:18 +0000

    Changed 'tokeniser' to 'tagger'. Added quote handling to tagger.

--- Deobfuscator.Tests/TokeniserTest.fs
+++ Deobfuscator.Tests/TokeniserTest.fs
@@ -23,19 +23,19 @@ type TestClass () =
                 | _ -> raise (ExBadShorthandInputException("Unknown leading symbol: " + chr.ToString()))
             | _ -> raise (ExBadShorthandInputException("Cannot create list with input: " + str))
         )
-        
 
 
     [<Test>]
     member this.SimpleTokenising() =
 
         let input    = "abc"
-        let actual   = tokenise input
+        let actual   = tag input
         let expected = [
             RegularChar('a') ; RegularChar('b') ; RegularChar('c')
         ]
 
         Assert.That(actual, Is.EqualTo(expected))
+
 
     [<Test>]
     member this.StandardCommandTokenising() =
@@ -50,12 +50,21 @@ type TestClass () =
             ("c^alc", ["Rc" ; "Ra" ; "Rl" ; "Rc"], "Escape tokens are dropped.")
             ("&", ["S&"], "Identify a known special char.")
             ("^&", ["R&"], "Escape a known special char.")
+            ("^^", ["R^"], "Escape the escape char.")
+            ("(foo)", ["S(" ; "Rf" ; "Ro" ; "Ro" ; "S)"], "Identify parens.")
+            (
+                "\"!()|?&%><\"",
+                ["S\"" ; "R!" ; "R(" ; "R)" ; "R|" ; "R?" ; "R&" ; "R%" ; "R>" ; "R<" ; "S\""],
+                "Quotes disable special char identification."
+            )
         ]
 
         tests |> List.iter (fun test ->
             let input, expected, msg = test
             let expectedTokens = this.ToTokenList expected
-            let actual = tokenise input
+            let actual = tag input
+
+            printfn "%A" actual
 
             Assert.That(actual, Is.EqualTo(expectedTokens), msg)
         )
--- Deobfuscator/Tokeniser.fs
+++ Deobfuscator/Tokeniser.fs
@@ -16,10 +16,9 @@ type TokenChar =
 
 module Tokeniser =
 
-    let (|SpecialChar|RegularChar|EscapeChar|) chr =
+    let (|SpecialChar|QuoteChar|RegularChar|EscapeChar|) chr =
         match chr with
         | ' '
-        | '"'
         | '('
         | ')'
         | '!'
@@ -28,30 +27,47 @@ module Tokeniser =
         | '&'
         | '|'
         | '\\' -> SpecialChar chr
+        | '"'  -> QuoteChar   chr
         | '^'  -> EscapeChar  chr
         | _    -> RegularChar chr
 
 
-    let rec private tokeniser (cmdstr: char list) (ctx: ReaderState) (col: TokenChar list) =
+    let rec private tagChars (cmdstr: char list) (ctx: ReaderState) (col: TokenChar list) =
         match cmdstr with
         | chr :: rest ->
             match chr with
+
+            | QuoteChar dquote when ctx.Mode = MatchingSpecialChars ->
+                tagChars rest { ctx with Escape = false ; Mode = IgnoringSpecialChars; } (List.append col [SpecialChar(chr)])
+
+            | QuoteChar dquote ->
+                tagChars rest { ctx with Mode = MatchingSpecialChars } (List.append col [SpecialChar(chr)])
+
+            | _ when ctx.Mode = IgnoringSpecialChars ->
+                tagChars rest ctx (List.append col [RegularChar(chr)])
+
+            | EscapeChar escape when ctx.Escape ->
+                tagChars rest { ctx with Escape = false } (List.append col [RegularChar(chr)])
+
             | EscapeChar escape ->
-                tokeniser rest { ctx with Escape = true } col
+                tagChars rest { ctx with Escape = true } col
 
             | _ when ctx.Escape ->
-                tokeniser rest { ctx with Escape = false} (List.append col [RegularChar(chr)])
+                tagChars rest { ctx with Escape = false} (List.append col [RegularChar(chr)])
 
             | SpecialChar special ->
-                tokeniser rest ctx (List.append col [SpecialChar(chr)])
+                tagChars rest ctx (List.append col [SpecialChar(chr)])
 
             | RegularChar regular ->
-                tokeniser rest ctx (List.append col [RegularChar(chr)])
+                tagChars rest ctx (List.append col [RegularChar(chr)])
         | _ ->
             col
 
-
-    let tokenise (cmdstr: string) =
+    /// <summary>Tagging is the first part of the tokenising process.  As the tagger
+    /// scans the input text, it classifies tokens in to two types: SpecialChars, and
+    /// RegularChars.  Once tagged, we feed these tokens in to the tokeniser, which will
+    /// take is from 'SpecialChar('&&')' to the token 'CALL_UPON_SUCCESS'.</summary>
+    let tag (cmdstr: string) =
         let cmdstrSeq = cmdstr.ToString() |> Seq.toList
-        tokeniser cmdstrSeq { Mode = MatchingSpecialChars; Escape = false } []
+        tagChars cmdstrSeq { Mode = MatchingSpecialChars; Escape = false } []
 
