d06616e119c8c4adaf06ec2bc0e50a3e21f9a35b
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2019-02-22 05:19:53 +0000

    Migrated 'scratch' Tokeniser code over in to Tokeniser.fs.  Now have 'str->AST' transforms.  Error handling TODO.

--- Deobfuscator/Tokeniser.fs
+++ Deobfuscator/Tokeniser.fs
@@ -1,18 +1,82 @@
+//               T O K E N I S E R
+//              ~-~-~-~-~-~-~-~-~-~
+//
+// HOW IT WORKS
+// ============
+//
+// The entry point to the tokeniser is the function `tokenise', which accepts
+// a single command string, though the real work (internal API) is all handled
+// by `tokeniseCmd'.
+//
+// Tokenising happens in a few different phases.  From a high-level, these are:
+//
+//   1. Split input string in to individual characters.
+//   2. For each char, assign it a Token type (LeftParen, Literal, Quote, ...)
+//   3. IF token is an operator, push on to the OPERATOR stack.
+//   4. IF token is an operand, push on to the OPERAND stack.
+//   5. When all tokens read, use shunting yard algorithm to build an AST.
+//
+//
+// OPERAND DATA STRUCTURES
+// ========================
+//
+// Operands include the set of all tokens, excluding:
+//
+//   * LEFT PAREN       (
+//   * RIGHT PAREN      )
+//   * COND ALWAYS      &
+//   * COND SUCCESS     &&
+//   * COND OR          ||
+//   * LEFT REDIRECT    >
+//   * RIGHT REDIRECT   <
+//
+// Because the early parts of the tokeniser is dealing with individual characters,
+// we need a mechanism of joining tokens together.  Attacked to the Token DU are two
+// useful members:
+//
+//   (+) - An overloaded + operator which concats tokens together, returning either a
+//         brand new token, or appending the char to an existing token.  The table below
+//         shows how inputs A and B are handled by this operator:
+//
+//         | Input Token A  | Input Token B  | Output Token     |
+//         |----------------|----------------|------------------|
+//         | Literal    "x" | Literal    "y" | Literal     "xy" |
+//         | Pipe       "|" | Pipe       "|" | CondOr      "||" |
+//         | CondAlways "&" | CondAlways "&" | CondSuccess "&&" |
+//
+//
+//   CanConcat - A 'gatekeeper' function which just checks whether tokenA and tokenB
+//               can be concatenated together.  Useful if you want to avoid writing
+//               exception handler code, as the overloaded '+' above will throw if
+//               given types which cannot be joined.
+//
+//
+// STACKS
+// ======
+//
+// During the tokenising phase, two stacks are used; one to hold OPERATORS, and another
+// to hold OPERANDS.  The OPERATOR stack is straightforward, while the OPERAND stack has
+// a slight complication.  The diagram below shows the two stacks, and their differences.
+// Essentially, OPERATORS are always of type Token, while OPERANDS are grouped together.
+//
+//             INPUT:     foo && bar | baz
+//
+//   After tokenisation, the stacks appear:
+//
+//                   OPERATORS                   OPERANDS
+//                +--------------+           +--------------+
+//                | PIPE         |           | [bar baz]    |
+//                +--------------+           +--------------+
+//                | COND SUCCESS |           | [foo]        |
+//                +--------------+           +--------------+
+//
+// Notice how the OPERANDS stack is grouped in to "commands", which are the instructions
+// between OPERATORS.
+//
 namespace Deobfuscator
 
 open System.Text.RegularExpressions
 
-//
-// TOKENISER
-//
-type private Matcher =
-    | MatchingSpecialChars
-    | IgnoringSpecialChars
-
-type private TokeniserState = {
-    Escape: bool
-    Mode: Matcher
-}
 
 type Token =
     | LeftParen     of string // (
@@ -43,6 +107,36 @@ type Token =
             | _ -> false
 
 
+type Stack =
+    | Operand
+    | Operator
+    | Neither
+
+
+type NodeType =
+    | Oper of Token
+    | Command  of Token list
+
+
+type Tree =
+    | Empty
+    | Node of value: NodeType * left: Tree * right: Tree
+
+
+type Matcher =
+    | MatchingSpecialChars
+    | IgnoringSpecialChars
+
+
+type TokeniserState = {
+    Escape: bool
+    Mode: Matcher
+    LastModifiedStack: Stack
+    OperandStack: Token list list
+    OperatorStack: Token list
+}
+
+
 module Tokeniser =
 
     let sameType a b = a.GetType() = b.GetType()
@@ -71,96 +165,135 @@ module Tokeniser =
         | ";" -> DELIM chr
         | _   -> OTHER chr
 
-    let appendToken (lst: Token list) tok =
 
-        let safeRest (lst: Token list) =
-            if lst.Length = 0 then []
-            else lst |> List.tail
+    let rec popN (stack: 'a list) n =
+        match stack with
+        | [] -> stack
+        | _ when n = 0 -> stack
+        | _ -> popN (stack.Tail) (n-1)
 
-        if lst.Length > 0 then
-            let prevTok = lst.Head
-            if Token.CanConcat(tok) && sameType tok prevTok then
-                (prevTok + tok) :: (lst |> safeRest)
+
+    let catStack (stack: Token list) =
+        if stack.Length > 1 then
+            let s0 = stack.[0]
+            let s1 = stack.[1]
+            if Token.CanConcat(s0) && sameType s0 s1 then
+                (s1 + s0) :: (popN stack 2)
             else
-                tok :: lst
+                stack
         else
-            tok :: lst
+            stack
 
 
-    let rec private tokeniseList (cmdstr: string list) (ctx: TokeniserState) acc =
+    let pushOperator (ctx: TokeniserState) (token: Token) =
+        (token :: ctx.OperatorStack) |> catStack
+
+
+    let pushOperand (ctx: TokeniserState) (token: Token) =
+
+        let operandsGroup = ctx.OperandStack.Head
+
+        if ctx.OperandStack.Length = 0 then
+            ((token :: operandsGroup) |> catStack) :: ctx.OperandStack.Tail
+        elif ctx.LastModifiedStack = Operand or ctx.LastModifiedStack = Neither then
+            // When the last modified stack is the operand stack, we shove this token
+            // in to the current group.
+            ((token :: operandsGroup) |> catStack) :: ctx.OperandStack.Tail
+        else
+            // The last thing we saw was an Operator, so start a new
+            // operands group...
+            [token] :: ctx.OperandStack
+
+
+    let rec tokeniseCmd (cmdstr: string list) (ctx: TokeniserState) =
 
         match cmdstr with
         | chr::rest ->
             match chr with
             | _ when ctx.Escape ->
-                tokeniseList rest {ctx with Escape=false} (appendToken acc (Literal chr))
+                let newStack = (pushOperand ctx (Literal chr))
+                tokeniseCmd rest {ctx with Escape=false; LastModifiedStack = Operand; OperandStack = newStack}
 
             | ESC _ when ctx.Mode = MatchingSpecialChars ->
-                tokeniseList rest {ctx with Escape=true} acc
+                tokeniseCmd rest {ctx with Escape=true}
 
             | QUOTE _ when ctx.Mode = MatchingSpecialChars ->
-                tokeniseList rest {ctx with Escape=false; Mode=IgnoringSpecialChars} (appendToken acc (Quote "\""))
+                let newStack = pushOperand ctx (Quote "\"")
+                tokeniseCmd rest {ctx with Escape=false; LastModifiedStack=Operand; Mode=IgnoringSpecialChars; OperandStack=newStack}
 
             | QUOTE _ ->
-                tokeniseList rest {ctx with Mode=MatchingSpecialChars} (appendToken acc (Quote "\""))
+                let newStack = pushOperand ctx (Quote "\"")
+                tokeniseCmd rest {ctx with Mode=MatchingSpecialChars; LastModifiedStack=Operand; OperandStack=newStack}
 
             | _ when ctx.Mode = IgnoringSpecialChars ->
-                tokeniseList rest ctx (appendToken acc (Literal(chr)))
+                let newStack = pushOperand ctx (Literal chr)
+                tokeniseCmd rest {ctx with OperandStack = newStack; LastModifiedStack=Operand}
 
             | LPAREN lp ->
-                tokeniseList rest ctx (appendToken acc (LeftParen lp))
+                let newStack = pushOperator ctx (LeftParen lp)
+                tokeniseCmd rest {ctx with OperatorStack = newStack; LastModifiedStack=Operator}
 
             | RPAREN rp ->
-                tokeniseList rest ctx (appendToken acc (RightParen rp))
+                let newStack = pushOperator ctx (RightParen rp)
+                tokeniseCmd rest {ctx with OperatorStack = newStack; LastModifiedStack=Operator}
 
             | LREDIRECT lrd ->
-                tokeniseList rest ctx (appendToken acc (LeftRedirect lrd))
+                let newStack = pushOperator ctx (LeftRedirect lrd)
+                tokeniseCmd rest {ctx with OperatorStack = newStack; LastModifiedStack=Operator}
 
             | RREDIRECT rrd ->
-                tokeniseList rest ctx (appendToken acc (RightRedirect rrd))
+                let newStack = pushOperator ctx (RightRedirect rrd)
+                tokeniseCmd rest {ctx with OperatorStack = newStack; LastModifiedStack=Operator}
 
             | DELIM delim ->
-                tokeniseList rest ctx (appendToken acc (Delimiter delim))
+                let newStack = pushOperand ctx (Delimiter delim)
+                tokeniseCmd rest {ctx with OperandStack = newStack; LastModifiedStack=Operand}
 
             | AMPERSAND amp ->
-                tokeniseList rest ctx (appendToken acc (CondAlways amp))
+                let newStack = pushOperator ctx (CondAlways amp)
+                tokeniseCmd rest {ctx with OperatorStack = newStack; LastModifiedStack=Operator}
 
             | PIPE pipe ->
-                tokeniseList rest ctx (appendToken acc (Pipe pipe))
+                let newStack = pushOperator ctx (Pipe pipe)
+                tokeniseCmd rest {ctx with OperatorStack = newStack; LastModifiedStack=Operator}
 
             | _ ->
-                tokeniseList rest ctx (appendToken acc (Literal chr))
+                let newStack = (pushOperand ctx (Literal chr))
+                tokeniseCmd rest {ctx with OperandStack = newStack; LastModifiedStack=Operand}
+        | _ -> ctx
 
-        | _ -> acc |> List.rev
+
+    let makeAST (operatorStack: Token list) (operandGroups: Token list list) =
+
+        let operandStack = operandGroups |> List.map (fun grp -> (Node ((Command (grp |> List.rev)), Empty, Empty)))
+
+        // Take one from operator stack, push on to operand stack.
+        let addNode (stack: Tree list) newThing =
+            let rhs = stack.[0]
+            let lhs = stack.[1]
+            let node = Node ((Oper newThing), lhs, rhs)
+            node :: (popN stack 2)
+
+        let rec mkAST operators (operands: Tree list) =
+            match operators with
+            | [] -> operands.Head
+            | _ ->
+                let x = (addNode operands operators.Head)
+                mkAST (operators.Tail) x
+
+        mkAST operatorStack operandStack
 
 
-    let tokenise cmdstr =
-        let state = { Escape = false; Mode = MatchingSpecialChars }
+    let tokenise (cmdstr: string) =
+        let state = {
+            Escape = false
+            Mode = MatchingSpecialChars
+            LastModifiedStack = Neither
+            OperandStack = [[]]; OperatorStack = []
+        }
+        let ast = Empty
         cmdstr.ToString()
-        |> Seq.toList
-        |> List.map (fun ch -> ch.ToString())
-        |> (fun x -> tokeniseList x state [])
-
-
-    let buildAST (tokens: Token list) =
-
-        let rec walk (lst: Token list) col acc =
-            printfn "----------------------------"
-            printfn "COL -> %A" col
-            printfn "ACC -> %A" acc
-            printfn "----------------------------"
-
-            match lst with
-            | head :: rest ->
-                match head with
-                | LeftParen  _ ->
-                    walk rest [] (col :: acc)
-
-                | RightParen _ ->
-                    walk rest [] (col :: acc)
-
-                | _ ->
-                    walk rest (head :: col) acc
-            | _ -> acc
-
-        walk tokens [] []
+            |> Seq.toList
+            |> List.map (fun ch -> ch.ToString())
+            |> (fun x -> tokeniseCmd x state)
+            |> (fun ctx -> makeAST ctx.OperatorStack ctx.OperandStack)
