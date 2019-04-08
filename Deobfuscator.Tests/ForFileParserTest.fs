8a0c1b5409aa5d98569fba32dd2663b0a279cce2
bobbystacksmash <33038262+bobbystacksmash@users.noreply.github.com>
2019-04-08 13:10:16 +0100

    Added for /f keyword parsing test cases.

--- Deobfuscator.Tests/ForFileParserTest.fs
+++ Deobfuscator.Tests/ForFileParserTest.fs
@@ -13,6 +13,48 @@ type TestClass () =
     [<Test>]
     member this.ParseForFKeywords() =
 
+        // Tests
+        // =====
+        //
+        // Error Tests
+        // -----------
+        // "skip=a"                     [a"] was unexpected at this time.
+       //  "eol=abc"                    [bc"]  was unexpected at this time.
+        // "eol"                        [eol"] was unexpected at this time.
+        // "eol=delims="                [elims="] was unexpected at this time.
+        // "delims=a b c"               [b c"] was unexpected at this time.
+        // "tokens=a"                   [a"] was unexpected at this time.
+        // "tokens=0a"                  [a"] was unexpected at this time.
+        // "tokens=0,3"                 [,3"] was unexpected at this time.
+        // "tokens=0"                   ["] was unexpected at this time.
+        // "tokens=1,0"                 ["] was unexpected at this time.
+        // 
+        // 
+        // 
+        //
+        // Successful Tests
+        // ----------------
+        // "tokens=1,2,3,4,5,6,7"       OK
+        // "tokens=0xa"                 OK
+        // "tokens=1"                   OK
+        // "tokens=03"                  OK
+        // "tokens=1,5*"                OK
+        // ""                           OK        
+        // "skip=0xa"                   OK
+        // "skip=07"                    OK
+        // "skip=1 skip=2"              OK
+        // "eol= delims="               OK
+        // "eol="                       OK        
+        // "eol= "                      OK        
+        // "eol= eol="                  OK
+        // "eol=a eol=b"                OK
+        // "delims=a delims=b"          OK
+        // "delims= delims="            OK
+        // "delims="                    OK
+        // "delims= "                   OK
+
+
+
         // for /f "delims=a b c" ... -> Error: 'b c' was unexpected at this time
         // for /f "delims=a^ b^ c" ... -> Error: '^b ^c' was unexpected at this time
 
